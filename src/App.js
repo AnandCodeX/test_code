c92f11a48927247fe91d60f38b98954a23ebcc6f
AnandCodeX <anandrtiwari@live.com>
2020-06-10 17:58:42 +0530

    Project_final_Build

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React, {Component} from 'react';
-import Test from './Components/Test';
-
-
+import React, { Component } from "react";
+import moduleName from "..//node_modules/bootstrap/dist/css/bootstrap.css";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
+import Contact from "./Components/Pages/Contact";
+import Navbar from "./Components/Layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotFound from "./Components/Pages/NotFound";
+import AddUser from "./Components/Users/AddUser";
+import EditUser from "./Components/Users/EditUser";
+import User from "./Components/Users/User";
 function App() {
   return (
-    <React.Fragment>
-      <h1>hello world from app</h1>
-    <Test/>
-    </React.Fragment>
-    
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/:id" component={User} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/Pages/About.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    
+      <div className="container">
+        <div className="py-4">
+          <h1>About page</h1>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim
+            quibusdam veniam officiis obcaecati exercitationem molestias ea
+            quaerat repudiandae ex laboriosam reiciendis ipsam impedit numquam
+            explicabo culpa distinctio, quod earum, incidunt recusandae
+            voluptatum asperiores odio ratione dolorum! Eveniet, neque?
+          </p>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim
+            quibusdam veniam officiis obcaecati exercitationem molestias ea
+            quaerat repudiandae ex laboriosam reiciendis ipsam impedit numquam
+            explicabo culpa distinctio, quod earum, incidunt recusandae
+            voluptatum asperiores odio ratione dolorum! Eveniet, neque?
+          </p>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim
+            quibusdam veniam officiis obcaecati exercitationem molestias ea
+            quaerat repudiandae ex laboriosam reiciendis ipsam impedit numquam
+            explicabo culpa distinctio, quod earum, incidunt recusandae
+            voluptatum asperiores odio ratione dolorum! Eveniet, neque?
+          </p>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim
+            quibusdam veniam officiis obcaecati exercitationem molestias ea
+            quaerat repudiandae ex laboriosam reiciendis ipsam impedit numquam
+            explicabo culpa distinctio, quod earum, incidunt recusandae
+            voluptatum asperiores odio ratione dolorum! Eveniet, neque?
+          </p>
+          <p className="lead">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni enim
+            quibusdam veniam officiis obcaecati exercitationem molestias ea
+            quaerat repudiandae ex laboriosam reiciendis ipsam impedit numquam
+            explicabo culpa distinctio, quod earum, incidunt recusandae
+            voluptatum asperiores odio ratione dolorum! Eveniet, neque?
+          </p>
+        </div>
+      </div>
+    
+  );
+};
+
+export default About;
